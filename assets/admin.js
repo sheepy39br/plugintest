face74bcbed40fa531c11daf563b8bd2d7f11ca3
Daniel Constantin <88145272+sheepy39br@users.noreply.github.com>
2025-08-18 15:03:38 +0300

    Delete assets directory

--- assets/admin.js
+++
@@ -1,6 +0,0 @@
-(function($){
-  $(document).on('click', '.ai-bgpro-topbar .mode-toggle', function(e){
-    e.preventDefault();
-    // (opțional) se poate face toggle temă via AJAX + user_meta
-  });
-})(jQuery);
